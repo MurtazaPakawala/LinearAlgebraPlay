db9d93d212b5504253d1bf71f766aa2fa8355e49
Murtaza Pakawala <mypaka52@gmail.com>
2023-05-08 23:13:31 +1000

    moving matrix to parent componenet

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import theme from './colortheme';
 import { ThemeProvider } from '@mui/material/styles';
 import Matrix from './Matrix';
+import CloseIcon from '@mui/icons-material/Close';
 const InnerGrid = ({ setMatrix, showMatrix }) => {
   let col = null;
   let row = null;
@@ -16,7 +17,14 @@ const InnerGrid = ({ setMatrix, showMatrix }) => {
   };
   const handleCreate = () => {
     if (row && col) {
-      setMatrix({ row, col });
+      const arr = Array(parseInt(row))
+        .fill()
+        .map(() =>
+          Array(parseInt(col))
+            .fill()
+            .map(() => Math.floor(Math.random() * 100))
+        );
+      setMatrix(arr);
       showMatrix(true);
     }
   };
@@ -54,10 +62,27 @@ const InnerGrid = ({ setMatrix, showMatrix }) => {
   );
 };
 const Main = () => {
-  const [matrixOne, setMatrixOne] = useState({ row: null, col: null });
-  const [matrixTwo, setMatrixTwo] = useState({ row: null, col: null });
+  const [matrixOne, setMatrixOne] = useState([]);
+  const [matrixTwo, setMatrixTwo] = useState([]);
   const [showMatrixOne, setShowMatrixOne] = useState(false);
   const [showMatrixTwo, setShowMatrixTwo] = useState(false);
+
+  const handleMultiply = () => {
+    // cant multuply if teh
+    if (matrixOne[0].length !== matrixTwo.length) {
+      alert('sorry but cannot multiply these matrices');
+      return;
+    }
+    // doing teh acutal multiplication
+    let res = Array(matrixOne.length);
+    matrixOne.forEach((row, index) => {
+      const resRow = [];
+      console.log('this is the row', row);
+      for (let i = 0; i < matrixTwo[0].length; i++) {
+        let sum = 0;
+      }
+    });
+  };
   return (
     <Grid container style={{ marginTop: 50 }}>
       <Grid container xs={6} spacing={3}>
@@ -66,18 +91,48 @@ const Main = () => {
       <Grid container xs={6} spacing={3}>
         <InnerGrid setMatrix={setMatrixTwo} showMatrix={setShowMatrixTwo} />
       </Grid>
-      {showMatrixOne && (
-        <Grid>
-          <Matrix row={matrixOne.row} col={matrixOne.col} />
-        </Grid>
-      )}
 
-      {showMatrixOne && showMatrixTwo && <h1>multiply</h1>}
-      {showMatrixTwo && (
-        <Grid>
-          <Matrix row={matrixTwo.row} col={matrixTwo.col} />
-        </Grid>
-      )}
+      <Grid container marginTop={2}>
+        {showMatrixOne && (
+          <Grid
+            item
+            xs={5}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+          >
+            <Matrix matrix={matrixOne} />
+          </Grid>
+        )}
+
+        {showMatrixOne && showMatrixTwo && (
+          <Grid
+            item
+            xs={2}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            flexDirection={'column'}
+          >
+            <CloseIcon sx={{ fontSize: 65 }} />
+
+            <Button variant='outlined' onClick={handleMultiply}>
+              Multiply
+            </Button>
+          </Grid>
+        )}
+        {showMatrixTwo && (
+          <Grid
+            item
+            xs={5}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+          >
+            <Matrix matrix={matrixTwo} />
+          </Grid>
+        )}
+      </Grid>
     </Grid>
   );
 };
--- src/Matrix.jsx
+++ src/Matrix.jsx
@@ -2,18 +2,7 @@ import { Button, Paper, ThemeProvider } from '@mui/material';
 import React, { useState } from 'react';
 import theme from './colortheme';
 
-const Matrix = ({ row, col }) => {
-  const arr = Array(parseInt(row))
-    .fill()
-    .map(() =>
-      Array(parseInt(col))
-        .fill()
-        .map(() => Math.floor(Math.random() * 100))
-    );
-
-  const [matrix, setMatrix] = useState(arr);
-  console.log(row, col);
-  console.log('see', matrix);
+const Matrix = ({ matrix }) => {
   return (
     <Paper elevation={1} square>
       {matrix.map((col) => (
