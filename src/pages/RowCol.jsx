5adef5da2b13dc102b657d1e48c1f0d7c869fa48
Murtaza Pakawala <mypaka52@gmail.com>
2023-05-12 00:31:47 +1000

    making it look good

--- src/pages/RowCol.jsx
+++ src/pages/RowCol.jsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from '@emotion/react';
-import { Button, Grid, Paper } from '@mui/material';
+import { Button, Grid, Paper, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import theme from '../colortheme';
@@ -13,27 +13,64 @@ const RowCol = () => {
   const [matrixTwoCol, setMatrixTwoCol] = useState(
     Array(matrixTwo.length * matrixTwo[0].length).fill(false)
   );
+  // this is the resultant matrix
+  const [resMatrix, setResMatrix] = useState(
+    Array(matrixOne.length)
+      .fill()
+      .map(() => Array(matrixTwo[0].length).fill(''))
+  );
+  // this is the res colour matrix
+  const [resMatrixCol, setResMatrixCol] = useState(
+    Array(resMatrix.length * resMatrix[0].length).fill(false)
+  );
   const startVisualizer = () => {
     // first need to color the
-    console.log('starting!!');
-    let delay = 1000;
-    for (let i = 0; i < matrixOne.length; i++) {
-      for (let k = 0; k < matrixTwo[0].length; k++) {
-        for (let j = 0; j < matrixOne[0].length; j++) {
-          setTimeout(() => {
+    // doing all stuff in an async
+    async function doColor() {
+      console.log('starting!!', resMatrix);
+      let delay = 1000;
+      for (let i = 0; i < matrixOne.length; i++) {
+        for (let k = 0; k < matrixTwo[0].length; k++) {
+          let sum = 0;
+          for (let j = 0; j < matrixOne[0].length; j++) {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
             // setting the first mtr
             const dummyMtrOne = [...matrixOneCol];
-            dummyMtrOne[i * matrixOne.length + j] = true;
+            dummyMtrOne[i * matrixOne[0].length + j] = true;
             // changing the mtr 2
             const dummyMtrTwo = [...matrixTwoCol];
-            dummyMtrTwo[j * matrixTwo.length + k] = true;
+            dummyMtrTwo[j * matrixTwo[0].length + k] = true;
+            // working with res matrix
+            const dummyMtrRes = [...resMatrixCol];
+            dummyMtrRes[i * matrixTwo[0].length + k] = true;
             setMatrixOneCol(dummyMtrOne); // set color of cell to red
             setMatrixTwoCol(dummyMtrTwo); // set color of cell to red
-          }, delay);
-          delay += 1000;
+            setResMatrixCol(dummyMtrRes);
+            // when is the element written in the result matrix
+            if (j === matrixOne[0].length - 1) {
+              console.log('this is the sum', sum);
+              const dummyRes = [...resMatrix];
+              dummyRes[i][k] = sum;
+              setResMatrix(dummyRes);
+            } else {
+              sum += matrixOne[i][j] * matrixTwo[j][k];
+            }
+          }
         }
       }
+      // when the loop is over turning everything to false again
+      setMatrixOneCol(Array(matrixOneCol.length).fill(false));
+      setMatrixTwoCol(Array(matrixTwoCol.length).fill(false));
+      setResMatrixCol(Array(resMatrixCol.length).fill(false));
+      console.log('completed coloring');
     }
+
+    // call the function and wrap it in a Promise
+    new Promise((resolve) => {
+      doColor().then(() => {
+        resolve();
+      });
+    });
   };
 
   useEffect(() => {
@@ -44,22 +81,63 @@ const RowCol = () => {
   }, []);
 
   return (
-    <Grid container>
-      <Grid item>
+    <Grid
+      container
+      spacing={0}
+      direction='row'
+      alignItems='center'
+      justifyContent='space-evenly'
+      style={{ minHeight: '80vh' }}
+    >
+      <Grid
+        item
+        display={'flex'}
+        gap={5}
+        direction={'column'}
+        alignItems={'center'}
+      >
+        <Typography>Matrix A</Typography>
         <InnerMatrixComp matrix={matrixOne} matrixCol={matrixOneCol} />
       </Grid>
-      <Grid item>
+      <Grid
+        item
+        display={'flex'}
+        gap={5}
+        direction={'column'}
+        alignItems={'center'}
+      >
+        <Typography>Matrix B</Typography>
         <InnerMatrixComp matrix={matrixTwo} matrixCol={matrixTwoCol} />
+      </Grid>
+      <Grid
+        item
+        display={'flex'}
+        gap={5}
+        direction={'column'}
+        alignItems={'center'}
+      >
+        <Typography>Resultant Matrix</Typography>
+        <InnerMatrixComp matrix={resMatrix} matrixCol={resMatrixCol} />
       </Grid>
     </Grid>
   );
 };
-
 const InnerMatrixComp = ({ matrix, matrixCol }) => {
   return (
-    <Paper elevation={1} square>
+    <Paper
+      sx={{
+        minHeight: 300,
+        minWidth: 300,
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'space-evenly',
+        justifyContent: 'space-evenly',
+      }}
+      elevation={1}
+      square
+    >
       {matrix.map((col, i) => (
-        <div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
           {col.map((elem, j) => (
             <ThemeProvider theme={theme}>
               {matrixCol[matrix[0].length * i + j] ? (
